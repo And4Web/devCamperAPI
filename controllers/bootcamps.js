9220d7f203cf008b3ff93de657c52895cdd12f94
And4Web <anands2807@gmail.com>
2022-11-09 16:15:58 +0530

    fetching bootcamp data all as well as single document done

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,15 +4,28 @@ const Bootcamp = require('../models/Bootcamp');
 //@desc     get all the bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   public
-exports.getBootcamps = (req, res, next)=>{
-  res.status(200).json({success: true, message: 'Show all bootcamps.'})
+exports.getBootcamps = async (req, res, next)=>{
+  try {
+    const bootcamps = await Bootcamp.find({}, {__v: 0, _id: 0});
+    res.status(200).json({success: true, data: bootcamps});
+  } catch (error) {
+   res.status(400).json({success: false, error: error.message});
+  }
 }
 
 //@desc     get a single bootcamp
 //@route    GET /api/v1/bootcamps/:id
 //@access   public
-exports.getBootcamp = (req, res, next)=>{
-  res.status(200).json({success: true, message: `Show single bootcamp ${req.params.id}.`})
+exports.getBootcamp = async (req, res, next)=>{
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if(!bootcamp){
+      return res.status(400).json({success: false, error: 'No bootcamp with that id exists.'})
+    }
+    res.status(200).json({success: true, data: bootcamp});
+  } catch (error) {
+    res.status(400).json({success: false, error: error.message});
+  }
 }
 
 //@desc     create a new bootcamp
@@ -23,7 +36,7 @@ exports.postBootcamp = async (req, res, next)=>{
     const bootcamp = await Bootcamp.create(req.body);  
     res.status(201).json({success: true, data: bootcamp});    
   } catch (error) {
-    res.status(400).json({success: false, message: error});
+    res.status(400).json({success: false, error: error.message});
   }
 
 }
