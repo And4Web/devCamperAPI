4ba57a3ac19297da4c60e87627ff057a7a3f8525
And4Web <anands2807@gmail.com>
2022-11-10 16:06:23 +0530

    validation error and duplication error handling done

--- middleware/error.js
+++ middleware/error.js
@@ -4,12 +4,23 @@ const errorHandler = (err, req, res, next) => {
   let error = {...err};
   error.message = err.message;
   //Log to console for developer
-  console.log(err.stack.red);
-
+  console.log(err);
   //Mongoose bad objectId
   if(err.name === 'CastError'){
     const message = `Bootcamp not found with the id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose Duplicate key
+  if(err.code === 11000){
+    const message = 'Duplicate field value entered.'
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose Validation error
+  if(err.name === 'ValidationError'){
+    const message = Object.values(err.errors).map(val=>val.message);
+    error = new ErrorResponse(message, 400);
   }
   
   res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'});
