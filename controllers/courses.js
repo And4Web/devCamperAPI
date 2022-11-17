bf0eaf02c7682a62454ece4090a12f8fb72a5ddb
And4Web <anands2807@gmail.com>
2022-11-17 15:00:12 +0530

    single course get controller created

--- controllers/courses.js
+++ controllers/courses.js
@@ -6,17 +6,35 @@ const Course = require("../models/Course");
 //@route    GET /api/v1/courses
 //@route    GET /api/v1/bootcamps/:bootcampId/courses
 //@access   public
+
 exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
-  if(req.params.bootcampId){
-    query = Course.find({bootcamp: req.params.bootcampId});    
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
     query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description averageCost'
-    });     
+      path: "bootcamp",
+      select: "name description averageCost",
+    });
   }
   const courses = await query;
 
-  res.status(200).json({success: true, count: courses.length, data: courses})
+  res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+//@desc     get a single course
+//@route    GET /api/v1/courses/:courseId
+//@access   private
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = Course.findById(req.params.courseId).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  })
+
+  if(!course){
+    return next(new ErrorResponse(`No course with the id ${req.params.courseId} found.`, 404))
+  }
+
+  res.status(200).json({success: true, data: course});
 })
